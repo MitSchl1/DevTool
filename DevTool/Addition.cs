6a6248569372a1ec9cca6c65112449a396c5ed34
mvolk <mvolk336@gmail.com>
2022-03-01 09:21:53 +0100

    Projektdateien hinzufügen.

---
+++ DevTool/Addition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTool
+{
+    internal class Addition : Calculation
+
+    {
+        private float result;
+        public override void Calc(float numberOne, float numberTwo)
+        {
+            result = numberOne + numberTwo;
+            Console.WriteLine(result);
+        }
+
+        public override void Definition()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DevTool/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DevTool;
+
+Console.WriteLine("Hello, World!");
+Addition add = new Addition();
+add.Calc(2, 3);
