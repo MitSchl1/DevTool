23048417f7cfadaddbfcc1aab5f63058c158a393
mvolk <mvolk336@gmail.com>
2022-03-01 09:30:15 +0100

    Subtraktion Division und Multiplikation hinzugefügt

---
+++ DevTool/Multiplication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTool
+{
+    internal class Multiplication : Calculation
+    {
+        private float result;
+        public override void Calc(float numberOne, float numberTwo)
+        {
+            result = numberOne * numberTwo;
+            Console.WriteLine(result);
+        }
+
+        public override void Definition()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
