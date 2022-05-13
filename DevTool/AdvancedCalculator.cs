8cb4811028e6cbca40d3112f576a20d469561b35
mvolk <mvolk336@gmail.com>
2022-05-13 12:03:52 +0200

    einführen eines Basic-& Advanced Calculator

---
+++ DevTool/AdvancedCalculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTool
+{
+    public class AdvancedCalculator : BasicCalculator
+    {
+        private Sin sin = new();
+        private Cos cos = new();
+        private Tan tan = new ();
+        private Log log = new ();
+        private Log10 log10 = new ();  
+        private Log2 log2 = new ();
+
+        public void SinCalc(float numberOne)
+        {
+            sin.CalcWithOnePassedValue(numberOne);
+        }
+        public void CosCalc(float numberOne)
+        {
+            cos.CalcWithOnePassedValue(numberOne);
+        }
+        public void TanCalc(float numberOne)
+        {
+            tan.CalcWithOnePassedValue(numberOne);
+        }
+        public void Log10Calc(float numberOne)
+        {
+            log10.CalcWithOnePassedValue(numberOne);
+        }
+        public void Log2Calc(float numberOne)
+        {
+            log2.CalcWithOnePassedValue(numberOne);
+        }
+        public void LogECalc(float numberOne)
+        {
+            log.CalcWithOnePassedValue(numberOne);
+        }
+        public void LogCalcToAnyBase(float numberOne, float numberTwo)
+        {
+            log.CalcWithTwoPassedValues(numberOne, numberTwo);
+        }
+    }
+}
---
+++ DevTool/BasicCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTool
+{
+    public class BasicCalculator 
+    {
+        private Addition add = new ();
+        private Substraction sub = new();
+        private Multiplication mult = new();
+        private Division div = new();
+
+
+        public void addCalc(float numberOne, float numberTwo)
+        {
+            add.CalcWithTwoPassedValues( numberOne, numberTwo);
+        }
+        public void subCalc(float numberOne, float numberTwo)
+        {
+            sub.CalcWithTwoPassedValues(numberOne, numberTwo);
+        }
+        public void multCalc(float numberOne, float numberTwo)
+        {
+            mult.CalcWithTwoPassedValues(numberOne, numberTwo);
+        }
+        public void divCalc(float numberOne, float numberTwo)
+        {
+            div.CalcWithTwoPassedValues(numberOne, numberTwo);
+        }
+    }
+}
