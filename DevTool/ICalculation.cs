555138f19244a8d3a136016583da595198e3c609
mvolk <mvolk336@gmail.com>
2022-05-13 09:59:45 +0200

    abstrakte Klasse Calculation durch Schnittstelle ICalculation ersetzt

---
+++ DevTool/ICalculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTool
+{
+    public interface ICalculation
+    {
+        public abstract void Definition();
+        public abstract void CalcWithTwoPassedValues(float numberOne, float numberTwo);
+
+        public abstract void CalcWithoutPassedValues();
+        public abstract void CalcWithOnePassedValue(float numberOne);
+    }
+}
