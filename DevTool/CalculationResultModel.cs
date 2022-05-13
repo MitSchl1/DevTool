d622d09b83d1d37e3a93cc3635e47835f28a5926
mvolk <mvolk336@gmail.com>
2022-05-13 10:06:12 +0200

    CalulationResultModel für DB Anbindung

---
+++ DevTool/CalculationResultModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTool
+{
+    public class CalculationResultModel
+    {
+        int id { get; set; }
+        string calculationName { get; set; }
+        float result { get; set; }
+
+    }
+}
