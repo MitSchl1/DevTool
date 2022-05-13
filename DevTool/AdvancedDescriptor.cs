737af6040ebca4441fe1991609bc931cc2153b0e
mvolk <mvolk336@gmail.com>
2022-05-13 12:04:19 +0200

    Einführen eines Basic- & Advanced Descriptors

---
+++ DevTool/AdvancedDescriptor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTool
+{
+    internal class AdvancedDescriptor : BasicDescriptor
+    {
+        private Sin sin = new();
+        private Cos cos = new();
+        private Tan tan = new();
+        private Log log = new();
+        private Log10 log10 = new();
+        private Log2 log2 = new();
+
+        public void SinDescription()
+        {
+            sin.Definition();
+        }
+        public void CosDescription()
+        {
+            cos.Definition();
+        }
+        public void TanDescription()
+        {
+            tan.Definition();
+        }
+        public void LogDescription()
+        {
+            log.Definition();
+        }
+        public void Log10Description()
+        {
+            log10.Definition();
+        }
+        public void Log2Description()
+        {
+            log2.Definition();
+        }
+
+    }
+}
---
+++ DevTool/BasicDescriptor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTool
+{
+    internal class BasicDescriptor
+    {
+        private Addition add = new();
+        private Substraction sub = new();
+        private Multiplication mult = new();
+        private Division div = new();
+
+        public void AddDescription()
+        {
+            add.Definition();
+        }
+        public void SubDescription()
+        {
+            sub.Definition();
+        }
+        public void MultDescription()
+        {
+            mult.Definition();
+        }
+        public void DivDescription()
+        {
+            div.Definition();
+        }
+    }
+}
