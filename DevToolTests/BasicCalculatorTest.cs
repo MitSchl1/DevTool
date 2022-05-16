4726ea102dd695bc481628e1a7f30d661e86d377
mvolk <mvolk336@gmail.com>
2022-05-16 11:25:05 +0200

    Diesmal Projekt wirklich testfähig gemacht promise

--- DevToolTests/BasicCalculatorTest.cs
+++ DevToolTests/BasicCalculatorTest.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
+using DevTool;
 
-namespace DevTool.UnitTests
+namespace DevToolTests
 {
     public class BasicCalculatorTest
     {
