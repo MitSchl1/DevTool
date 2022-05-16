fad1b71aa4b20b7e4db172404789a0b342f327f0
mvolk <mvolk336@gmail.com>
2022-05-16 10:27:44 +0200

    Basic Calculation tests

---
+++ DevTool/UnitTests/BasicCalculatorTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevTool.UnitTests
+{
+    public class BasicCalculatorTest
+    {
+        BasicCalculator basicCalculator = new();
+        
+        [Fact]
+        public void Addition_Test()
+        {
+            //Act
+            double actual = basicCalculator.addCalc(2, 2);
+
+            //Arrange
+            double expected = 4;    
+
+            //Assert
+
+            Assert.Equal(expected, actual);
+
+        }
+        [Fact]
+        public void Subtraction_Test()
+        {
+            //Act
+            double actual = basicCalculator.subCalc(2, 5);
+
+            //Arrange
+            double expected = -3;
+
+            //Assert
+
+            Assert.Equal(expected, actual);
+
+        }
+        [Fact]
+        public void Multiplication_Test()
+        {
+            //Act
+            double actual = basicCalculator.multCalc(2, 3);
+
+            //Arrange
+            double expected = 6;
+
+            //Assert
+
+            Assert.Equal(expected, actual);
+
+        }
+        [Fact]
+        public void Division_Test()
+        {
+            //Act
+            double actual = basicCalculator.divCalc(2, 2);
+
+            //Arrange
+            double expected = 1;
+
+            //Assert
+
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
