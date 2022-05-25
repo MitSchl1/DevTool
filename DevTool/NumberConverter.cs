5a4052b65112b0ca7b53d136b42c6f54771b1d8c
mvolk <mvolk336@gmail.com>
2022-05-25 15:37:36 +0200

    NumbrConverter hinzugefügt

---
+++ DevTool/NumberConverter.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTool
+{
+    public class NumberConverter
+    {
+
+
+        public string ConvertDecToBin(int number)
+        {
+            string binNumber = string.Empty;
+            int remainder;
+            while (number > 0)
+            {
+                remainder = number % 2;
+                number /= 2;
+                binNumber = remainder.ToString() + binNumber;
+            }
+            return binNumber;
+        }
+
+        public string ConvertDecToTern(int number)
+        {
+            string binNumber = string.Empty;
+            int remainder;
+            while (number > 0)
+            {
+                remainder = number % 3;
+                number /= 3;
+                binNumber = remainder.ToString() + binNumber;
+            }
+            return binNumber;
+        }
+        public string ConvertDecToQuatern(int number)
+        {
+            string binNumber = string.Empty;
+            int remainder;
+            while (number > 0)
+            {
+                remainder = number % 4;
+                number /= 4;
+                binNumber = remainder.ToString() + binNumber;
+            }
+            return binNumber;
+        }
+        public string ConvertDecToQuin(int number)
+        {
+            string binNumber = string.Empty;
+            int remainder;
+            while (number > 0)
+            {
+                remainder = number % 5;
+                number /= 5;
+                binNumber = remainder.ToString() + binNumber;
+            }
+            return binNumber;
+        }
+        public string ConvertDecToSen(int number)
+        {
+            string binNumber = string.Empty;
+            int remainder;
+            while (number > 0)
+            {
+                remainder = number % 6;
+                number /= 6;
+                binNumber = remainder.ToString() + binNumber;
+            }
+            return binNumber;
+        }
+        public string ConvertDecToSepten(int number)
+        {
+            string binNumber = string.Empty;
+            int remainder;
+            while (number > 0)
+            {
+                remainder = number % 7;
+                number /= 7;
+                binNumber = remainder.ToString() + binNumber;
+            }
+            return binNumber;
+        }
+        public string ConvertDecToOktal(int number)
+        {
+            string binNumber = string.Empty;
+            int remainder;
+            while (number > 0)
+            {
+                remainder = number % 8;
+                number /= 8;
+                binNumber = remainder.ToString() + binNumber;
+            }
+            return binNumber;
+        }
+        public string ConvertDecToNon(int number)
+        {
+            string binNumber = string.Empty;
+            int remainder;
+            while (number > 0)
+            {
+                remainder = number % 9;
+                number /= 9;
+                binNumber = remainder.ToString() + binNumber;
+            }
+            return binNumber;
+        }
+
+
+        public void ConvertDecToBinDescription()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ConvertDecToTernDescription()
+        {
+            throw new NotImplementedException();
+
+        }
+        public string ConvertDecToQuaternDescription()
+        {
+            throw new NotImplementedException();
+
+        }
+        public string ConvertDecToQuinDescription()
+        {
+            throw new NotImplementedException();
+
+        }
+        public string ConvertDecToSenDescription()
+        {
+            throw new NotImplementedException();
+
+        }
+        public string ConvertDecToSeptenDescription()
+        {
+            throw new NotImplementedException();
+
+        }
+        public string ConvertDecToOktalDescription()
+        {
+            throw new NotImplementedException();
+
+        }
+        public string ConvertDecToNonDescription()
+        {
+            throw new NotImplementedException();
+
+        }
+
+    }
+}
